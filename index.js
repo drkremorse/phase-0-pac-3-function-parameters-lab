8cdef677d89432ae622000b0dbb9e8141e8b5f1f
Josh Carrion <joshcarrion92@gmail.com>
2022-08-18 00:32:46 -0400

    commit tree e77128f04d0306ec14be0269c5a78541cf67df38

---
+++ index.js
@@ -0,0 +1,6 @@
+function introduction(name) {
+    console.log(`Hi, my name is ${name}.`);
+}
+function introductionWithLanguage(name, language) {
+    console.log(`Hi, my name is ${name} and i am learning to program in ${language}.`;
+}
